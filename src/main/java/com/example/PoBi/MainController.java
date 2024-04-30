3f3dbdc9625b078ed280accd14f667136023ea8c
Jincoco <chiomilkchio@gmail.com>
2024-04-30 16:47:30 +0900

    h2-database add

--- src/main/java/com/example/PoBi/MainController.java
+++ src/main/java/com/example/PoBi/MainController.java
@@ -1,4 +1,16 @@
 package com.example.PoBi;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/pobi")
+    @ResponseBody
+    public String index(){
+        return "PoBi!!";
+    }
 }
