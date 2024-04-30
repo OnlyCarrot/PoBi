f5747190cd005f55543b6eaf66c048f331bfa7d2
Jincoco <chiomilkchio@gmail.com>
2024-04-30 17:09:04 +0900

    upload web

--- src/main/java/com/example/PoBi/MainController.java
+++ src/main/java/com/example/PoBi/MainController.java
@@ -2,15 +2,12 @@ package com.example.PoBi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/pobi")
-    @ResponseBody
-    public String index(){
-        return "PoBi!!";
+    @GetMapping("/")
+    public String root(){
+        return "main";
     }
 }
